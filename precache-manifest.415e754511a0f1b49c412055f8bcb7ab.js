6d1d818bb5fa9dfa2562f24d6f045adc3059b2c2
unknown <mihai.sereanu@gmail.com>
2020-10-13 17:52:01 +0300

    Updates

---
+++ precache-manifest.415e754511a0f1b49c412055f8bcb7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c609e69f049e33753a317edee54461",
+    "url": "/react_top10/index.html"
+  },
+  {
+    "revision": "958971cfbb7038d2544a",
+    "url": "/react_top10/static/css/main.ee840c6f.chunk.css"
+  },
+  {
+    "revision": "c064abb645b3b720bbcb",
+    "url": "/react_top10/static/js/2.e9d6e3aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_top10/static/js/2.e9d6e3aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958971cfbb7038d2544a",
+    "url": "/react_top10/static/js/main.e8642599.chunk.js"
+  },
+  {
+    "revision": "cd9d000976dc16e2ccc8",
+    "url": "/react_top10/static/js/runtime-main.031728f4.js"
+  }
+]);
